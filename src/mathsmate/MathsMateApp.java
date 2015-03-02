8f09e815a907412cac18baabeb43fa259bb60f3d
Reccy <aaronmeaney@hotmail.com>
2015-03-02 19:37:35 +0000

    Finished Main Menu GUI

--- src/mathsmate/MathsMateApp.java
+++ src/mathsmate/MathsMateApp.java
@@ -5,6 +5,7 @@ package mathsmate;
  */
 public class MathsMateApp {
     public static void main(String[] args) {
-        
+        MainMenu m = new MainMenu();
+        m.setVisible(true);
     }
 }
